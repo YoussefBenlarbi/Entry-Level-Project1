72b6fc01eabeada6d1b11a45da8350c1d68cc125
Youssef <benlarbi1youssef@gmail.com>
2022-10-31 17:55:44 +0100

    deleting everything

--- app-mangement/src/components/module.js
+++
@@ -1,8 +0,0 @@
-function Module({nomModule,moyenne,coe}){
-    // const {nomModule} = props ;
-    return (
-        <li >{nomModule} {" : "} {moyenne}{" / Coefficient: "} {coe} </li>
-    )
-}
-
-export default Module;
